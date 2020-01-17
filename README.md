26db247eb32987d7a3040a2817c550dfa99d38b1
Zalbani <pro.pierson.alban@gmail.com>
2020-01-17 12:11:05 +0100

    Init app

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# API
+# Project Nook API
+
+School project aiming at the creation of a platform for event countdown
+
+## Production site
+
+Deploy with heroku
+
+```In progress```
+
+## Getting Started 
+
+1. Create and fill the `.env` file like the exemple `.env.example`
+
+2. Install depedencies
+```npm install```
+
+3. Launch the app
+```nodemon server```
+
+### Prerequisites
+
+- NodeJS
+- MongoDB database
+
+## Authors
+
+* **Alexis Capdepon** - [Alexis](https://github.com/AlexisCapdepon)
+* **Alban Pierson** - [Zalbani](https://github.com/Zalbani)
+* **Fabien Treve** - [Fabien](https://github.com/FabienTreve)
---
+++ controllers/event.js
@@ -0,0 +1,35 @@
+const Event = require('../models/Event');
+
+exports.createEvent = (req, res) => {
+    const event = new Event({
+        ...req.body
+    });
+    event.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteEvent =  (req, res) => {
+    Event.deleteOne({_id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprime !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.updateEvent = (req,res) => {
+    Event.updateOne({_id: req.params.id },
+        { ...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet modifie !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneEvent = (req, res)=>{
+    Event.findOne({_id: req.params.id})
+        .then(event => res.status(200).json(event))
+        .catch(error => res.status(404).json({error}));
+};
+
+exports.getAllEvents =  (req, res) => {
+    Event.find()
+        .then(events => res.status(200).json(events))
+        .catch(error => res.status(400).json({ error }));
+};
