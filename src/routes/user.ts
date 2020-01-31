01a73612e953f77d4af5666b337b1de2571a756e
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-01-31 10:35:27 +0100

    add multer middleware / add multer to routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,12 @@
 const  express = require('express');
 const router = express.Router();
 
+const multer = require('../middleware/multer-config');
+
 const userCtrl = require('../controllers/user');
 
 router.get('/', userCtrl.getAllUsers);
-router.post('/', userCtrl.createUser);
+router.post('/', multer, userCtrl.createUser);
 router.get('/:id', userCtrl.getOneUser);
 router.delete('/:id', userCtrl.deleteUser);
 router.put('/:id',  userCtrl .updateUser);
