89e1d6c84d6aa7d2866610f2ee409099649cc924
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-01-31 09:22:50 +0100

    user model update

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,14 @@ const eventSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  email:{
+    type: String,
+    required: true,
+  },
+  password:{
+    type: String,
+    required: true,
+  },
   avatar:{
     type: String,
     required: true,
