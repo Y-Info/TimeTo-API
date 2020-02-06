b9da974131b354970773863a6829f7cecce27fea
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-02-06 11:23:19 +0100

    add role process

---
+++ src/routes/role.ts
@@ -0,0 +1,21 @@
+const  express = require('express');
+const router = express.Router();
+
+const roleCtrl = require('../controllers/role');
+
+router.get('/', auth, roleCtrl.getAllRoles);
+router.post('/', auth, roleCtrl.updateRole);
+router.get('/:id',auth, roleCtrl.getOneRole);
+router.delete('/:id',auth,roleCtrl.deleteRole);
+router.put('/:id', auth, roleCtrl.updateRole);
+
+export const EventRouter = router;
+
+
+
+
+
+
+
+
+
