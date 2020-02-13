4110fd28bf0771829e5c873c9daf9fe68ebdecff
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-02-13 15:15:38 +0100

    error message update

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -12,7 +12,7 @@ exports.signup = (req, res, next) => {
       });
       user.save()
       .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-      .catch(error => res.status(400).json({ messageError:error.message}));
+      .catch(error => res.status(400).json(error.message));
     })
     .catch(error => res.status(500).json(error.message ));
 }
