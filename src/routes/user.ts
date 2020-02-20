575bb60c0487c8525149a160dd7beba3a6730856
Zalbani <pro.pierson.alban@gmail.com>
2020-02-20 17:00:00 +0100

    Hot fix update user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -10,7 +10,7 @@ router.get('/', auth, userCtrl.getAllUsers);
 router.post('/', auth, userCtrl.createUser);
 router.get('/:id', auth, userCtrl.getOneUser);
 router.delete('/:id', auth, userCtrl.deleteUser);
-router.put('/:id',  auth, userCtrl .updateUser);
+router.put('/:id',  auth, userCtrl.updateUser);
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.post('/image', auth, multer, userCtrl.getUrlImage);
