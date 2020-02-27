25536c5f1fb51a106cac5a66f2c26fa180d75bb5
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-02-27 14:52:41 +0100

    add ts-ignor

--- src/middleware/adminAuth.ts
+++ src/middleware/adminAuth.ts
@@ -1,9 +1,10 @@
-const jwtt = require('jsonwebtoken');
+// @ts-ignore
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwtt.verify(token, process.env.RANDOM_TOKEN_SECRET);
+    const decodedToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
     const userRole = decodedToken.userRole;
       if(userRole === 'admin'){
         next();
